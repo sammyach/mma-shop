d18ce266800c03433f43d6ce7d440495678b2ad1
sammyach <sammyachman@gmail.com>
2022-04-14 12:46:18 +0100

    modify apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:44362',
+  apiUrl: 'https://localhost:44331', //https://mmawebapi.somee.com
   baseImgUrl: 'assets/img/'
 };
